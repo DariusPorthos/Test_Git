dd6639c80c3b32349ec2d7b65f08aff280851335
DariusPorthos <119850378+DariusPorthos@users.noreply.github.com>
2023-01-11 19:30:32 +0100

    test

---
+++ View/view_begin_membre.php
@@ -0,0 +1,36 @@
+<!Doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="Content/CSS/style.css">
+    <title>BDE USPN</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<body>
+<header>
+<nav>
+    <ul id="container">
+        <li class="li_gauche"><a href="?controller=list&action=last"><img id="logoBDE" src="../Content/img/Logo_BDE_noir.png"></a> </li>
+        <li class="li_gauche"><a href="?controller=list&action=last">Accueuil</a></li>
+        <li class="li_gauche"><a href="?controller=commande&action=commande">Commande</a></li>
+        <li class="li_gauche"><a href="?controller=list&action=last">Inventaire</a></li>
+        <li class="li_gauche"><a href="?controller=list&action=last">Comptabilite</a></li>
+        <li class="li_gauche"><a href="?controller=list&action=last">Gestion Compte</a></li>
+        <li id="droite"><a href="?controller=list&action=last">About</a></li>
+    </ul>
+
+</nav>
+</header>
+
+<main>
+    <a href="view_commande.php" style="width: 10rem">
+    <div class="card" style="width: 10rem; background-color: grey">
+        <img src="../Content/img/Logo_BDE_noir.png" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text align-items-center-center">150€</p>
+
+        </div>
+    </div>
+    </a>
+</main>
---
+++ View/view_home.php
@@ -0,0 +1,20 @@
+
+    <?php
+    require "view_header_client.php";
+    ?>
+</header>
+
+<section class="accueil" id="#accueil">
+    <div class="content">
+        <span>Bienvenue</span>
+        <h3></h3>
+    </div>
+</section>
+
+    </header>
+
+
+<?php
+require "view_end.php";
+?>
+
